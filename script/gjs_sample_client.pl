c35ac4bc3cca7504be627fb8fb49b476b9a3d05c
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2014-02-03 18:15:11 +0200

    Remove the non-working examples

--- script/gjs_sample_client.pl
+++ script/gjs_sample_client.pl
@@ -18,72 +18,11 @@ use Data::Dumper;
 
 sub main()
 {
-	#
-	# Server administration example
-	#
-	my $config = NinetyNineBottlesOfBeer->configuration;
-
-	say STDERR 'server_version(): ' . Dumper(Gearman::JobScheduler::Admin::server_version($config));
-	say STDERR 'server_verbose(): ' . Dumper(Gearman::JobScheduler::Admin::server_verbose($config));
-	say STDERR 'create_function(): ' . Dumper(Gearman::JobScheduler::Admin::create_function('wooooooo!', $config));
-	say STDERR 'drop_function(): ' . Dumper(Gearman::JobScheduler::Admin::drop_function('wooooooo!', $config));
-
-	say STDERR 'show_jobs(): ' . Dumper(Gearman::JobScheduler::Admin::show_jobs($config));
-	say STDERR 'show_unique_jobs(): ' . Dumper(Gearman::JobScheduler::Admin::show_unique_jobs($config));
-	say STDERR 'cancel_job(): ' . Dumper(Gearman::JobScheduler::Admin::cancel_job('H:tundra.local:17', $config));
-
-	say STDERR 'get_pid(): ' . Dumper(Gearman::JobScheduler::Admin::get_pid($config));
-	# say STDERR 'shutdown(): ' . Dumper(Gearman::JobScheduler::Admin::shutdown($config));
-
-	say STDERR 'status(): ' . Dumper(Gearman::JobScheduler::Admin::status($config));
-	say STDERR 'workers(): ' . Dumper(Gearman::JobScheduler::Admin::workers($config));
-
-
-	#
-	# Client example
-	#
-	my $result;
-	my $gearman_job_id;
-	my $operand_a = 2;
-	my $operand_b = 3;
-
-	say STDERR "Will add two numbers (locally)";
-	$result = Addition->run_locally({a => $operand_a, b => $operand_b});
-	say STDERR "Result of adding $operand_a to $operand_b: $result";
-
-	say STDERR "Will add two numbers on Gearman";
-	$result = Addition->run_locally({a => $operand_a, b => $operand_b});
-	say STDERR "Result of adding $operand_a to $operand_b: $result";
-
-	say STDERR "Will enqueue adding two numbers on Gearman";
-	$gearman_job_id = Addition->enqueue_on_gearman({a => $operand_a, b => $operand_b});
-	say STDERR "Gearman job ID: $gearman_job_id";
-	eval {
-		say STDERR "Status of the addition job: " . Dumper(Gearman::JobScheduler::job_status(Addition->name(), $gearman_job_id));
-		sleep(1);
-		say STDERR "Status of the addition job after 1 second: " . Dumper(Gearman::JobScheduler::job_status(Addition->name(), $gearman_job_id));
-		say STDERR "Log path to the addition job: " . Dumper(Gearman::JobScheduler::log_path_for_gearman_job(Addition->name(), $gearman_job_id));
-	};
-	if ($@) {
-		say STDERR "log_path_for_gearman_job() failed, probably the job isn't running as of now: $@";
+	for (my $x = 1; $x < 100; ++$x) {
+		say STDERR "Will enqueue NinetyNineBottlesOfBeer on Gearman";
+		my $gearman_job_id = NinetyNineBottlesOfBeer->enqueue_on_gearman({how_many_bottles => $x});
+		say STDERR "Gearman job ID: $gearman_job_id";
 	}
-
-	# Failing job
-	say STDERR "Will run a failing job locally";
-	eval {
-		$result = AdditionAlwaysFails->run_on_gearman({a => $operand_a, b => $operand_b});
-	};
-	if ($@) {
-		say STDERR "Addition job failed because: $@";
-	} else {
-		die "Well, that's unexpected.";
-	}
-
-	say STDERR "Will enqueue a failing job on Gearman";
-	eval {
-		$result = AdditionAlwaysFails->enqueue_on_gearman({a => $operand_a, b => $operand_b});
-	};
-
 }
 
 main();
