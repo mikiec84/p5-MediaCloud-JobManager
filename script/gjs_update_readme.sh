0137e6629436da1490a6eafaf226028765a5c278
Linas Valiukas <lvaliukas@cyber.law.harvard.edu>
2016-04-01 16:40:05 +0300

    Remove unused script [ci skip]

--- script/gjs_update_readme.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo -n > README.mdown
-echo -n "[![Build Status](https://travis-ci.org/pypt/p5-Gearman-JobScheduler.svg?branch=master)](https://travis-ci.org/pypt/p5-Gearman-JobScheduler)" >> README.mdown
-echo -n " " >> README.mdown
-echo -n "[![Coverage Status](https://coveralls.io/repos/pypt/p5-Gearman-JobScheduler/badge.png?branch=master)](https://coveralls.io/r/pypt/p5-Gearman-JobScheduler?branch=master)" >> README.mdown
-echo >> README.mdown
-echo >> README.mdown
-pod2markdown lib/Gearman/JobScheduler/AbstractFunction.pm >> README.mdown
